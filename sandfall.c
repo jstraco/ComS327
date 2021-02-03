89665ddb8db041a1d82fc3531bef47bcd6f99610
Student <jstraco@gmail.com>
2021-02-02 18:07:22 -0600

    help

--- sandfall.c
+++ sandfall.c
@@ -13,17 +13,22 @@ void printArray(int arr[][MAX]){
     }
     printf("\n");
   }
-  
+  printf("\n");
 }
 
-void updatePlane(int arr[][MAX]){
+void topple(int arr[][MAX], int x, int y){
   int i,j;
-  for(j=0;j<MAX;j++){
-    for(i=0;i<MAX;i++){
-      printf("%d", arr[j][i]);
+  for (j=y-1 ;j < y+2 ; j++){
+    for(i=x-1 ;i < x+2 ; i++){
+
+      if(i >= 0 && i <= MAX && j >=0 && j <= MAX){
+	arr[j][i]++;
+	if(arr[j][i] > 8){
+	  topple(arr, j, i); 
+	}
+      }
     }
-    printf("\n");
-  }
+  } 
 }
 
 int main(/*int argc, char *argv[]*/) {
@@ -32,11 +37,17 @@ int main(/*int argc, char *argv[]*/) {
   //something
   int plane[MAX][MAX];
   int i,j;
+  //populating array with 0s
   for(j=0;j<MAX;j++){
     for(i=0;i<MAX;i++){
       plane[j][i] = 0;
     }
   }
+  
+  //printArray(plane);
+  plane[11][11]=8;
+  printArray(plane);
+  topple(plane, 11, 11);
   printArray(plane);
   /*
   for (j = 0 ; j < 10000 ; j++) {
