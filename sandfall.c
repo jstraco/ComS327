e08152e1001e53e63209d64dcca8f9017d86945d
Student <jstraco@gmail.com>
2021-02-04 13:13:38 -0600

    Good to submit?

--- sandfall.c
+++ sandfall.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #define MAX 23
 
+//method to print the plane
 void printArray(int arr[][MAX]){
   int i,j;
   for(j=0;j<MAX;j++){
@@ -17,7 +19,7 @@ void printArray(int arr[][MAX]){
   }
   printf("\n");
 }
-
+//method to cause a pile of >8 to topple, handles other piles that might topple recursivly
 void topple(int arr[][MAX], int y, int x){
   int i,j;
   arr[y][x] = 0;
@@ -32,29 +34,25 @@ void topple(int arr[][MAX], int y, int x){
     }
   } 
 }
-
-
-int highest(int first, int second);
-int lowest(int first, int second);
-
-/* TO DO
-   impliment sleep time. I think we need to get user input for what they want the refresh rate to be; so you might need to pass another variable into the "topple" method for the refresh rate
- */
 int main(int argc, char *argv[]) {
-
-  int j, i;
-  //definitions
+  //declarations
+  int j, i, fps;
+  int argIndex = 1;
   int plane[MAX][MAX];
-  //populating array with 0, TODO: take user input from the command line
-  //and populate the desired positions of the plane with the desired
-  //values that the user has input, "./sandpile 11 11 8" would make the center
-  //value at 11 11 be equal to 8.
+  //for loop to populate the plane with zeros
   for(j=0;j<MAX;j++){
     for(i=0;i<MAX;i++){
       plane[j][i] = 0;
     }
   }
-  int argIndex = 1; 
+  //taking user input from command line
+  if (!strcmp("--fps", argv[1])){
+    fps = atoi(argv[2]);
+    argIndex = 3;
+    argc = argc - 2;
+  } else {
+    fps = 60;
+  }
   for(i=argc/3;i>0;i--) {
     plane[atoi(argv[argIndex + 1])][atoi(argv[argIndex])]  = atoi(argv[argIndex + 2]);
     argIndex += 3;
@@ -68,6 +66,8 @@ int main(int argc, char *argv[]) {
       topple(plane, 11, 11);
     }
     printArray(plane);
+    printf("FPS = %d\n", fps);
+    usleep(100000/fps);
   }
   return 0;
 }
