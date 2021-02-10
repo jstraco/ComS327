7f23307ec0b730b55c9195cef883ffba683a95b9
Student <jstraco@gmail.com>
2021-02-09 18:59:09 -0600

    pointer boi, i need help

--- project1.01/dungeonGen.c
+++ project1.01/dungeonGen.c
@@ -4,7 +4,7 @@
 #define Y 21
 #define X 80
 
-
+//This should simply print the dungeon
 void printDungeon(char *dungeon[][X]){
    for(int j = 0; j < X ;j++){
      for(int i = 0; i < Y;i++){
@@ -13,10 +13,12 @@ void printDungeon(char *dungeon[][X]){
      printf("\n");
    }
 }
+//I'm not sure if I did the pointers right
 int main(int argc, char *argv[]) {
 
   char *dungeon[Y][X];
-
+  //this is just adding a top boarder and a bottom boarder of --- to the dungeon,
+  //with white space in the middle
   for(int j = 0; j < X ;j++){
     *dungeon[0][j] = '-';
     for(int i = 1; i < Y-1;i++){
