ed868c12575f355e35facbfc4b11473d4fff6193
nicholas soultz <ndsoultz@iastate.edu>
2021-02-11 00:18:41 -0600

    pushed stuff

--- project1.01/dungeonGen.c
+++ project1.01/dungeonGen.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define Y 21
 #define X 80
+//this is the array of charecters for the X by Y dungeon
+char dungeon[Y][X];
+void generateRooms();
 
 struct room
 {
@@ -10,36 +14,103 @@ struct room
   int yPos;
   int xSize;
   int ySize;
+  int xEndPoint;
+  int yEndPoint;
+  int yMid;
+  int xMid;
 };
+struct room rooms[6];
 
-void generateRooms(struct room rooms[6]){
-  //https://www.geeksforgeeks.org/generating-random-number-range-c/ (random num gen)
-  // todo generate rooms of random size to fit room size requirements 
-  for(int i = 0; i < 6 ; i++){
-    printf("%d",i);
+void addRooms(struct room rooms[6]){
+  printf("room %d y value %d, x value %d \n",0,rooms[0].yPos,rooms[0].xPos);
+  for(int currentRoom = 0; currentRoom < 6; currentRoom++){
+    for(int currentY = rooms[currentRoom].yPos; currentY <= rooms[currentRoom].yEndPoint; currentY++){
+      for(int currentX = rooms[currentRoom].xPos; currentX <= rooms[currentRoom].xEndPoint; currentX++){
+	dungeon[currentY][currentX] = '.';
+      }
+    }
+    printf("room %d y value %d, x value %d \n",0,rooms[0].yPos,rooms[0].xPos);
+    printf("room %d (%d,%d) to (%d,%d)\n",currentRoom,rooms[currentRoom].yPos,rooms[currentRoom].xPos,rooms[currentRoom].yEndPoint,rooms[currentRoom].xEndPoint);
   }
 }
 
-//This should simply print the dungeon
+void checkRooms(struct room rooms[6]){
+  for(int i = 0; i < 6; i++){
+    printf("room %d (%d,%d) to (%d,%d)\n",i,rooms[i].yPos,rooms[i].xPos,rooms[i].yEndPoint,rooms[i].xEndPoint);
+    for(int j = 1; j < 6; j++){
+      if(i == j && i == 5){
+	break;
+      }
+      else{
+	if(rooms[i].xPos == rooms[j].xPos || rooms[i].yPos == rooms[j].yPos){
+	  generateRooms();
+	  }
+	 else if(rooms[i].xPos <= rooms[j].xPos && rooms[j].xPos <= rooms[i].xEndPoint && rooms[i].yPos <= rooms[j].yPos && rooms[j].yPos <= rooms[i].yEndPoint){
+	  generateRooms();
+	}
+	 else if(rooms[i].xPos >= rooms[j].xPos && rooms[j].xPos >= rooms[i].xEndPoint && rooms[i].yPos >= rooms[j].yPos && rooms[j].yPos >= rooms[i].yEndPoint){
+	  generateRooms();
+	}
+      }
+    }
+  }
+}
+
+void generateRooms(){
+  
+  srand(time(0));
+
+  rooms[0].xPos = (rand() % 74) +1;
+  rooms[0].yPos = (rand() % 16) +1;
+
+  do {
+    rooms[0].xSize = (rand() % 7) +4;
+    rooms[0].ySize = (rand() % 8) +3;
+  } while (rooms[0].xPos + rooms[0].xSize < X && rooms[0].yPos + rooms[0].ySize < Y);
+  
+  rooms[0].xEndPoint = rooms[0].xPos + rooms[0].xSize;
+  rooms[0].yEndPoint = rooms[0].yPos + rooms[0].ySize;
+
+  //set midpoint
+  
+  for(int i = 1; i < 6 ; i++){
+
+    do{
+    rooms[i].xPos = (rand() % 74) +1;
+    rooms[i].yPos = (rand() % 16) +1;
+    } while (checkRooms(i));
+    rooms[i].xSize = (rand() % (10 - 4 + 1)) +4;
+    rooms[i].ySize = (rand() % (10 - 3 + 1)) +3;
+    
+    rooms[i].xEndPoint = rooms[i].xPos + rooms[i].xSize;
+    rooms[i].yEndPoint = rooms[i].yPos + rooms[i].ySize;
+    if(checkRooms(i));
+  }
+}
+
+
+
+  int checkRooms(int i)
+  {
+    for(int j = 0; j < i; j++) {
+      if(rooms[j].xPos <= rooms[j].xPos && rooms[i].xEndpoint >= rooms[currIndex].x
+	 return 0;
+    }
+      return 1;
+  }
+
+//This will  simply print the dungeon
 void printDungeon(char dungeon[][X]){
    for(int j = 0; j < Y ;j++){
      for(int i = 0; i < X;i++){
-       printf("%c", dungeon[j][i]);
+        printf("%c", dungeon[j][i]);
      }
-     printf("\n");
+      printf("\n");
    }
 }
-//I'm not sure if I did the pointers right
+
 int main(int argc, char *argv[]) {
-  //struct room rooms[6];
-
-   char dungeon[Y][X];
-
-   //generateRooms(rooms);
-
-   //printf("%d",rooms[0].xPos);
-   //dungeon[1][1] =* "g";
-  //does this need pointers?
+   //The following for loops will populate the dungeon with empty space and boarders
    for(int j = 0; j < Y ;j++){
      dungeon[j][0] = '|';
      dungeon[j][X-1] = '|';
@@ -51,6 +122,9 @@ int main(int argc, char *argv[]) {
      dungeon[0][i] = '-';
      dungeon[Y-1][i] = '-';
    }
+   generateRooms();
+   checkRooms(rooms);
+   addRooms(rooms);
   printDungeon(dungeon);
   return 0;
 }
