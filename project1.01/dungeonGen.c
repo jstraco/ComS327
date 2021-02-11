44e33463d60ff79fad4016243d03b4de017b7436
Student <jstraco@gmail.com>
2021-02-11 04:07:14 -0600

    fuck

--- project1.01/dungeonGen.c
+++ project1.01/dungeonGen.c
@@ -7,6 +7,8 @@
 //this is the array of charecters for the X by Y dungeon
 char dungeon[Y][X];
 void generateRooms();
+int checkPoint();
+int checkSize();
 
 struct room
 {
@@ -23,18 +25,19 @@ struct room rooms[6];
 
 void addRooms(struct room rooms[6]){
   printf("room %d y value %d, x value %d \n",0,rooms[0].yPos,rooms[0].xPos);
-  for(int currentRoom = 0; currentRoom < 6; currentRoom++){
-    for(int currentY = rooms[currentRoom].yPos; currentY <= rooms[currentRoom].yEndPoint; currentY++){
-      for(int currentX = rooms[currentRoom].xPos; currentX <= rooms[currentRoom].xEndPoint; currentX++){
-	dungeon[currentY][currentX] = '.';
+  for(int room = 0; room < 6; room++){
+    for(int j = rooms[room].yPos; j <= rooms[room].yEndPoint; j++){
+      for(int i = rooms[room].xPos; i <= rooms[room].xEndPoint; i++){
+	dungeon[j][i] = '.';
+	//dungeon[j][i] = room+0;
       }
     }
-    printf("room %d y value %d, x value %d \n",0,rooms[0].yPos,rooms[0].xPos);
-    printf("room %d (%d,%d) to (%d,%d)\n",currentRoom,rooms[currentRoom].yPos,rooms[currentRoom].xPos,rooms[currentRoom].yEndPoint,rooms[currentRoom].xEndPoint);
+    //printf("room %d y value %d, x value %d \n",0,rooms[0].yPos,rooms[0].xPos);
+    printf("room %d (%d,%d) to (%d,%d)\n",room,rooms[room].yPos,rooms[room].xPos,rooms[room].yEndPoint,rooms[room].xEndPoint);
   }
 }
 
-void checkRooms(struct room rooms[6]){
+/*void checkRooms(struct room rooms[6]){
   for(int i = 0; i < 6; i++){
     printf("room %d (%d,%d) to (%d,%d)\n",i,rooms[i].yPos,rooms[i].xPos,rooms[i].yEndPoint,rooms[i].xEndPoint);
     for(int j = 1; j < 6; j++){
@@ -55,7 +58,7 @@ void checkRooms(struct room rooms[6]){
     }
   }
 }
-
+*/
 void generateRooms(){
   
   srand(time(0));
@@ -66,64 +69,80 @@ void generateRooms(){
   do {
     rooms[0].xSize = (rand() % 7) +4;
     rooms[0].ySize = (rand() % 8) +3;
-  } while (rooms[0].xPos + rooms[0].xSize < X && rooms[0].yPos + rooms[0].ySize < Y);
+    rooms[0].xEndPoint = rooms[0].xPos + rooms[0].xSize;
+    rooms[0].yEndPoint = rooms[0].yPos + rooms[0].ySize;
+    //printf("made it here %d %d\n",rooms[0].xEndPoint,rooms[0].yEndPoint );
+  } while (rooms[0].xEndPoint >= X && rooms[0].yEndPoint >= Y);
   
-  rooms[0].xEndPoint = rooms[0].xPos + rooms[0].xSize;
-  rooms[0].yEndPoint = rooms[0].yPos + rooms[0].ySize;
-
   //set midpoint
   
   for(int i = 1; i < 6 ; i++){
 
-    do{
+    do {
     rooms[i].xPos = (rand() % 74) +1;
     rooms[i].yPos = (rand() % 16) +1;
-    } while (checkRooms(i));
+    printf("Make It Here2!\n");
+    } while (checkPoint(i));
+
+    do {
     rooms[i].xSize = (rand() % (10 - 4 + 1)) +4;
     rooms[i].ySize = (rand() % (10 - 3 + 1)) +3;
-    
     rooms[i].xEndPoint = rooms[i].xPos + rooms[i].xSize;
     rooms[i].yEndPoint = rooms[i].yPos + rooms[i].ySize;
-    if(checkRooms(i));
+    printf("Make It Here3!\n");
+    } while (checkSize(i));
   }
 }
-
-
-
-  int checkRooms(int i)
+//This is solid
+int checkPoint(int i){
+    for(int j = 0; j < i; j++) {
+      if(rooms[i].xPos > rooms[j].xPos - 5 && rooms[i].xPos < rooms[j].xEndPoint + 1 &&
+	  rooms[i].yPos > rooms[j].yPos - 4 && rooms[i].yPos < rooms[j].yEndPoint + 1){
+	printf("Make It Here66666666!       %d\n", i);
+	return 1;
+      }
+    }
+    return 0;
+}
+//This is also solid
+int checkSize(int i)
   {
     for(int j = 0; j < i; j++) {
-      if(rooms[j].xPos <= rooms[j].xPos && rooms[i].xEndpoint >= rooms[currIndex].x
-	 return 0;
+      if(rooms[i].xEndPoint >= X || rooms[i].yEndPoint >= Y || 
+	 ((rooms[i].xEndPoint > rooms[j].xPos - 1 && rooms[i].xPos < rooms[j].xPos) &&
+	 (rooms[i].yEndPoint > rooms[j].yPos - 1 && rooms[i].yPos < rooms[j].yPos)))
+	 return 1;
     }
-      return 1;
+      return 0;
   }
 
 //This will  simply print the dungeon
 void printDungeon(char dungeon[][X]){
+  for(int i = 0; i < 82; i++){
+    printf("-");
+  }
+  printf("\n");
    for(int j = 0; j < Y ;j++){
+     printf("|");
      for(int i = 0; i < X;i++){
         printf("%c", dungeon[j][i]);
      }
-      printf("\n");
+      printf("|\n");
    }
+  for(int i = 0; i < 82; i++){
+    printf("-");
+  }
+  printf("\n");
 }
 
 int main(int argc, char *argv[]) {
    //The following for loops will populate the dungeon with empty space and boarders
-   for(int j = 0; j < Y ;j++){
-     dungeon[j][0] = '|';
-     dungeon[j][X-1] = '|';
-     for(int i = 1; i < X-1;i++){
+     for(int j = 0; j < Y ;j++){
+       for(int i = 0; i < X;i++){
        dungeon[j][i] = ' ';
      }
    }
-   for(int i = 0; i < X; i++){
-     dungeon[0][i] = '-';
-     dungeon[Y-1][i] = '-';
-   }
    generateRooms();
-   checkRooms(rooms);
    addRooms(rooms);
   printDungeon(dungeon);
   return 0;
