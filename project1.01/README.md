e6e33723c1fc6006d8cc1f699f152220257751fe
nicholas soultz <ndsoultz@iastate.edu>
2021-02-11 22:09:55 -0600

    added finishing touches

---
+++ project1.01/README.md
@@ -0,0 +1,9 @@
+# ComS327
+Jadyn Gust Jagust@iastate.edu
+Matt Karmelich matkarm@iastate.edu
+Nick Soultz Ndsoultz@iastate.edu
+
+CHANGELOG
+2/06/21 "Room Creation"
+2/08/21 "Finished testing valid rooms"
+2/11/21 "added finish touches with staircases and tunnels"
--- project1.01/dungeonGen.c
+++ project1.01/dungeonGen.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #define Y 21
 #define X 80
+#define numberOfRooms 7
 //this is the array of charecters for the X by Y dungeon
 char dungeon[Y][X];
 void generateRooms();
@@ -21,16 +22,19 @@ struct room
   int yMid;
   int xMid;
 };
-struct room rooms[6];
+struct room rooms[numberOfRooms];
 
-void addRooms(struct room rooms[6]){
-  for(int room = 0; room < 6; room++){
+void addRooms(struct room rooms[numberOfRooms]){
+  for(int room = 0; room < numberOfRooms; room++){
     for(int j = rooms[room].yPos; j <= rooms[room].yEndPoint; j++){
       for(int i = rooms[room].xPos; i <= rooms[room].xEndPoint; i++){
+	if(rooms[room].xPos >=100 || rooms[room].yPos >= 100 || rooms[room].xEndPoint >= 100 || rooms[room].yEndPoint >= 100){
+	  generateRooms();
+	}
 	dungeon[j][i] = '.';
       }
     }
-    printf("room %d (%d,%d) to (%d,%d)\n",room,rooms[room].yPos,rooms[room].xPos,rooms[room].yEndPoint,rooms[room].xEndPoint);
+    printf("test room %d (%d,%d) to (%d,%d)\n",room,rooms[room].yPos,rooms[room].xPos,rooms[room].yEndPoint,rooms[room].xEndPoint);
   }
 }
 
@@ -48,7 +52,7 @@ void generateRooms(){
     rooms[0].yEndPoint = rooms[0].yPos + rooms[0].ySize;
   } while (rooms[0].xEndPoint >= X && rooms[0].yEndPoint >= Y);
   
-  for(int i = 1; i < 6 ; i++){
+  for(int i = 1; i < numberOfRooms ; i++){
 
     do {
     rooms[i].xPos = (rand() % 74) +1;
@@ -84,6 +88,69 @@ int checkSize(int i)
     }
       return 0;
   }
+
+void xDrill(int start, int end, int yHeight){
+  for(int i = start; i <= end; i++){
+    if(dungeon[yHeight][i] !='.'){
+	dungeon[yHeight][i] = '#';
+      }
+  }
+}
+  void yDrill(int start, int end, int xHeight){
+    for(int i = start; i <= end; i++){
+      if(dungeon[i][xHeight] != '.'){
+	dungeon[i][xHeight] = '#';
+      }
+    }
+}
+  void addTunnels(){
+    for(int i = 0; i < numberOfRooms; i++){
+      if(i == numberOfRooms-1){
+	if(rooms[0].xPos < rooms[numberOfRooms-1].xPos){
+	  xDrill(rooms[0].xPos,rooms[numberOfRooms-1].xPos,rooms[0].yPos);
+	    if(rooms[0].yPos < rooms[numberOfRooms-1].yPos){
+	      yDrill(rooms[0].yPos,rooms[numberOfRooms-1].yPos,rooms[numberOfRooms-1].xPos);
+	    }
+	    else{
+	      yDrill(rooms[numberOfRooms-1].yPos,rooms[0].yPos,rooms[numberOfRooms-1].xPos);
+	    }
+	}
+	else{
+	  xDrill(rooms[numberOfRooms-1].xPos,rooms[0].xPos,rooms[numberOfRooms-1].yPos);
+	  if(rooms[0].yPos < rooms[numberOfRooms-1].yPos){
+	    yDrill(rooms[0].yPos,rooms[numberOfRooms-1].yPos,rooms[0].xPos);
+	  }
+	  else{
+	    yDrill(rooms[numberOfRooms-1].yPos,rooms[0].yPos,rooms[0].xPos);
+	  }
+	}
+      }
+      else{
+	if(rooms[i].xPos < rooms[i+1].xPos){
+	  xDrill(rooms[i].xPos,rooms[i+1].xPos,rooms[i].yPos);
+	  if(rooms[i].yPos < rooms[i+1].yPos){
+	    yDrill(rooms[i].yPos,rooms[i+1].yPos,rooms[i+1].xPos);
+	  }
+	  else{
+	    yDrill(rooms[i+1].yPos,rooms[i].yPos,rooms[i+1].xPos);
+	  }
+	}
+	else{
+	  xDrill(rooms[i+1].xPos,rooms[i].xPos,rooms[i+1].yPos);
+	  if(rooms[i].yPos < rooms[i+1].yPos){
+	    yDrill(rooms[i].yPos,rooms[i+1].yPos,rooms[i].xPos);
+	  }
+	  else{
+	    yDrill(rooms[i+1].yPos,rooms[i].yPos,rooms[i].xPos);
+	  }
+	}
+      }
+    }
+  }
+void addStairs(){
+  dungeon[(rooms[0].yPos + (rooms[0].ySize)/2)][(rooms[0].xPos + (rooms[0].xSize)/4)] = '>';
+  dungeon[(rooms[5].yEndPoint - (rooms[5].ySize)/3)][(rooms[5].xEndPoint - (rooms[5].xSize)/4)] = '<';
+}
 //This will  simply print the dungeon
 void printDungeon(char dungeon[][X]){
   for(int i = 0; i < 82; i++){
@@ -110,8 +177,10 @@ int main(int argc, char *argv[]) {
        dungeon[j][i] = ' ';
      }
    }
-   generateRooms();
-   addRooms(rooms);
+  generateRooms();
+  addRooms(rooms);
+  addTunnels();
+  addStairs();
   printDungeon(dungeon);
   return 0;
 }
