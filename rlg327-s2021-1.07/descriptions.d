885148c2fdf587d399064c8ecc26ad979d27d3ef
jstraco <jstraco@gmail.com>
2021-04-06 22:41:16 -0500

    lets go bouys

---
+++ rlg327-s2021-1.07/descriptions.d
@@ -0,0 +1,2 @@
+descriptions.o: descriptions.cpp descriptions.h dice.h dungeon.h heap.h \
+ dims.h character.h npc.h utils.h
---
+++ rlg327-s2021-1.07/path.d
@@ -0,0 +1,2 @@
+path.o: path.cpp path.h dungeon.h heap.h dims.h character.h \
+ descriptions.h dice.h utils.h pc.h
